3387d723b9f032686cafb46fd57b34fa1b787b4b
Elliott Record <elliottrecord@gmail.com>
2021-09-26 13:02:21 +0100

    Added models for todo_list and todo_item

--- backend/todo-actix/src/models.rs
+++ backend/todo-actix/src/models.rs
@@ -1,7 +1,24 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use tokio_pg_mapper_derive::PostgresMapper;
 
 #[derive(Serialize)]
-pub struct Status{
+pub struct Status {
     pub status: String,
-    pub number: u8
+    pub number: u8,
+}
+
+#[derive(Serialize, Deserialize, PostgresMapper)]
+#[pg_mapper(table = "todo_list")]
+pub struct TodoList {
+    pub id: i32,
+    pub title: String,
+}
+
+#[derive(Serialize, Deserialize, PostgresMapper)]
+#[pg_mapper(table = "todo_item")]
+pub struct TodoItem {
+    pub id: i32,
+    pub title: String,
+    pub checked: bool,
+    pub list_id: i32,
 }
