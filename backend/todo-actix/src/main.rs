593d744a608616f22e719db0fbdf950a71f3dd42
Elliott Record <elliottrecord@gmail.com>
2021-09-26 13:02:41 +0100

    Added /todos route and config to main app

--- backend/todo-actix/src/main.rs
+++ backend/todo-actix/src/main.rs
@@ -1,20 +1,32 @@
+mod config;
+mod db;
+mod handlers;
 mod models;
-use actix_web::{HttpServer, App, web, Responder};
-use std::io;
-use models::Status;
 
-async fn status() -> impl Responder{
-    web::Json(Status{status:"Ok".to_string(), number: 12})
-}
+use crate::handlers::*;
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use std::io;
+use tokio_postgres::NoTls;
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(||{
+    dotenv().ok();
+    let config = crate::config::Config::from_env().unwrap();
+
+    let pool = config.pg.create_pool(NoTls).unwrap();
+
+    println!(
+        "Server started on http://{}:{}/",
+        config.server.host, config.server.port
+    );
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
             .route("/", web::get().to(status))
-            
+            .route("/todos{_:/?}", web::get().to(get_todos))
     })
-    .bind("0.0.0.0:5000")?
-            .run()
-            .await
+    .bind(format!("{}:{}", config.server.host, config.server.port))?
+    .run()
+    .await
 }
